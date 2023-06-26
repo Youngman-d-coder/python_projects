62ca61bf523c6fa796cf598e0332daf9d4333db0
Nelson Chimdiadi <64903808+Youngman-d-coder@users.noreply.github.com>
2023-06-26 17:07:18 +0100

    update

---
+++ python_by_example/part_1/maths/033.py
@@ -0,0 +1,7 @@
+num_1 = int(input("Enter a number :  "))
+num_2 = int(input("Enter a number :  "))
+
+division = num_1 // num_2
+remainder = num_1 % num_2
+
+print(f"{num_1} divided by {num_2} is {division} with a remainder of {remainder}")
