af0f09e100fe6a8dd6d94ed1c62419dba64287ef
Nelson <sayhitonelson@gmail.com>
2023-06-30 12:52:07 +0000

    update

---
+++ python_by_example/part_1/for_loop/036.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+num = int(input("Enter the number of times you want to print your name:  "))
+
+for i in range(num):
+	print(name)
---
+++ python_by_example/part_1/for_loop/038.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+num = int(input("Enter the number of times you want to print your name:  "))
+
+for i in range(num):
+	for i in name:
+		print(i)
